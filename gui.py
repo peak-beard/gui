be5c2d30e22bfebeb66c790e22f62b16c3ff20eb
peak-beard <2330358@learn71.ca>
2019-11-13 10:17:10 -0800

    learned how buttons work

--- gui.py
+++ gui.py
@@ -2,5 +2,10 @@ import tkinter as tk
 
 window = tk.Tk()
 window.title('GUI')
-label = tk.Label(window, text='Hello World!').pack()
+def clicked():
+
+    label = tk.Label(window, text='Hello World!', font=('Arial Bold', 12)).grid(column=0, row=0)
+
+bt = tk.Button(window, text="Enter", bg='grey', fg='white').grid(column=1, row=0)
+window.geometry('350x200')
 window.mainloop()
