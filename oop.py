98ea6cbed92f34b3c72b1dbe7006f71de37eaaa5
peak-beard <2330358@learn71.ca>
2019-11-20 09:35:32 -0800

    oop practise with https://www.youtube.com/watch?v=A0gaXfM1UN0&list=PLQVvvaa0QuDclKx-QpC9wntnURXVJqLyk&index=2

---
+++ oop.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+
+class SeaofBTCapp(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+        
+        tk.Tk.__init__(self, *args, **kwargs)
+        container = tk.Frame(self)
+
